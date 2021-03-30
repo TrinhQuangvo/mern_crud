4edddd984b466ec511694abc1d670b1779d8f9bc
Quang.Fe.Dev <voquang1406@gmail.com>
2021-03-30 20:06:10 +0700

    like

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -46,3 +46,20 @@ export const deletePost = async (req, res) => {
 
   res.json({ message: "Delete Success Fully!" });
 };
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+  console.log(req.params);
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send("No Post Found !");
+  }
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    {
+      likeCount: post.likeCount + 1,
+    },
+    { new: true }
+  );
+  res.json(updatePost);
+};
