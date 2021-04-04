f84a057b277ad7b6d75b8bc62469be9afae890c9
Quang.Fe.Dev <voquang1406@gmail.com>
2021-04-05 00:25:19 +0700

    fix bugs

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,12 +2,13 @@ import jwt from "jsonwebtoken";
 
 const auth = async (req, res, next) => {
   try {
+    console.log(req.headers);
     const token = req.headers.authorization.split(" ")[1];
     const isCustomAuth = token.length < 500;
     let decodeData;
 
     if (token && isCustomAuth) {
-      decodeData = jwt.verify(token, "test");
+      decodeData = jwt.decode(token);
       req.userId = decodeData?.id;
     } else {
       decodeData = jwt.decode(token);
